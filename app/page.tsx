52eafe56f1d2b7bcf9ae1d57604cd862d1029d17
pappukrs <digitalpappu101@gmail.com>
2026-01-08 22:19:40 +0530

    again changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <div>
     <h1>Nextjs  Ec2 DEployment</h1>
+    <h1>Testing for Redployment</h1>
+    </div>
   );
 }
